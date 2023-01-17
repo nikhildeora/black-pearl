f8c3b375c142b2d3331b0106342cd5aff3ac0fd8
aayyuusshh21 <aayyuusshh21@gmail.com>
2023-01-17 21:17:43 +0530

    Created homepage

---
+++ src/components/Homepage/Homepage.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/navigation";
+import { Pagination, Navigation } from "swiper";
+import slider1 from "../../assets/Homepage/slider1-1.jpg"
+const Homepage = () => {
+  return (
+    <div>
+      <Swiper
+        slidesPerView={1}
+        spaceBetween={30}
+        loop={true}
+        pagination={{
+          clickable: true,
+        }}
+        navigation={true}
+        modules={[Pagination, Navigation]}
+        className="mySwiper"
+      >
+        <SwiperSlide>
+          <img
+            src={slider1}
+            alt=""
+          />
+        </SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SwiperSlide>Slide 5</SwiperSlide>
+        <SwiperSlide>Slide 6</SwiperSlide>
+        <SwiperSlide>Slide 7</SwiperSlide>
+        <SwiperSlide>Slide 8</SwiperSlide>
+        <SwiperSlide>Slide 9</SwiperSlide>
+      </Swiper>
+      {/* <img src="" alt="" /> */}
+    </div>
+  );
+};
+
+export default Homepage;
