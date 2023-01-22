eafa657811dea21e1ecf45643609b65be324f386
aayyuusshh21 <aayyuusshh21@gmail.com>
2023-01-22 18:01:32 +0530

    address and payment

---
+++ src/components/Address And Payment/Address.jsx
@@ -0,0 +1,81 @@
+import React, { useRef, useState } from "react";
+import styles from "./Address.module.css";
+const Address = () => {
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const addreddRef = useRef(null);
+  const cityRef = useRef(null);
+  const statelRef = useRef(null);
+
+  const getFormData = () => {
+    if (
+      nameRef.current.value == "" ||
+      emailRef.current.value == "" ||
+      addreddRef.current.value == "" ||
+      cityRef.current.value == "" ||
+      statelRef.current.value == ""
+    ) {
+      // console.log(addressObj);
+      alert("Please fill Complete Address");
+    }
+
+    let addressObj = {
+      fullName: nameRef.current.value,
+      email: emailRef.current.value,
+      address: addreddRef.current.value,
+      city: cityRef.current.value,
+      state: statelRef.current.value,
+    };
+
+    console.log(addressObj);
+    localStorage.setItem("addreddObj", JSON.stringify(addressObj));
+
+    // console.log();
+  };
+
+  return (
+    <div className={styles.addressMain}>
+      <h1>Billing Address</h1>
+      <div className={styles.addressInput}>
+        <p>Full Name</p>
+        <input
+          name="fullName"
+          type="text"
+          placeholder="Full Name"
+          ref={nameRef}
+        />
+        <p>Email</p>
+        <input type="email" name="email" placeholder="Email" ref={emailRef} />
+        <p>Address</p>
+        <input
+          type="text"
+          name="address"
+          placeholder="Full Address"
+          ref={addreddRef}
+        />
+        <p>City</p>
+        <input type="text" name="city" placeholder="City" ref={cityRef} />
+        <select name="state" ref={statelRef}>
+          <option value="">Choose State..</option>
+          <option value="Rajasthan">Rajasthan</option>
+          <option value="Jharkhand">Jharkhand</option>
+          <option value="Delhi">Delhi</option>
+          <option value="Uttar Pradesh">Uttar Pradesh</option>
+          <option value="Haryana">Haryana</option>
+          <option value="Gujrat">Gujrat</option>
+          <option value="Uttrakhand">Uttrakhand</option>
+          <option value="New Delhi">New Delhi</option>
+          <option value="Maharashtra">Maharashtra</option>
+          <option value="Karnataka">Karnataka</option>
+          <option value="Nagaland">Nagaland</option>
+          <option value="Meghalaya">Meghalaya</option>
+          <option value="Punjab">Punjab</option>
+          <option value="Tamil Nadu">Tamil Nadu</option>
+        </select>
+      </div>
+      <button onClick={() => getFormData()}>Continue to Payment</button>
+    </div>
+  );
+};
+
+export default Address;
---
+++ src/components/Address And Payment/Payment.jsx
@@ -0,0 +1,72 @@
+import React, { useState } from "react";
+import styles from "./Payment.module.css";
+const Payment = () => {
+  const [cardNumber, setCardNumber] = useState("");
+  const [expirationDate, setExpirationDate] = useState("");
+  const [cvv, setCvv] = useState("");
+  const [amount, setAmount] = useState("");
+
+  const addressObj = JSON.parse(localStorage.getItem("addreddObj"));
+  //   console.log(addressObj.fullName);
+
+  const handleSubmit = async (event) => {
+    console.log("hi");
+  };
+  return (
+    <div className={styles.mainPayment}>
+      <div className={styles.mainPaymentInputs}>
+        <p>Hi {addressObj.fullName}</p>
+        <p>Your Product will be delivered to</p>
+        <p>{addressObj.address}</p>
+        <p style={{ color: "red" }}>Thank you for Shopping with us</p>
+      </div>
+
+      <div className={styles.cardaPymentNew}>
+        <button>Cash On Delivery?</button>
+        <h2>Online Payment</h2>
+        <div className={styles.cardPaymentInputs}>
+          <p>Name on the card</p>
+          <input type="text" />
+          <p>Card Number</p>
+          <input
+            type="number"
+            maxlength="16"
+            placeholder="Enter your card number"
+          />
+          <p style={{ margin: "1rem 0" }}>Expire </p>
+          <select>
+            <option value="">Month</option>
+            <option value="1">January - 01</option>
+            <option value="2">February - 02</option>
+            <option value="3">March - 03</option>
+            <option value="4">April - 04</option>
+            <option value="5">May - 05</option>
+            <option value="6">June - 06</option>
+            <option value="7">July - 07</option>
+            <option value="8">August - 08</option>
+            <option value="9">September - 09</option>
+            <option value="10">October - 10</option>
+            <option value="11">November - 11</option>
+            <option value="12">December - 12</option>
+          </select>
+          <select>
+            <option value="">Year</option>
+            <option value="2023">2023</option>
+            <option value="2204">2024</option>
+            <option value="2025">2025</option>
+            <option value="2026">2026</option>
+            <option value="2027">2027</option>
+            <option value="2028">2028</option>
+            <option value="2029">2029</option>
+            <option value="2030">2030</option>
+          </select>
+          <p>CVV</p>
+          <input type="tel" maxLength={3} placeholder="***" />
+        </div>
+        <button>Pay</button>
+      </div>
+    </div>
+  );
+};
+
+export default Payment;
